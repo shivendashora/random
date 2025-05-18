e992ec9020a5e60cbac6124711128cec36afbb45
Shiven Dashora <shiven.dashora@gmail.com>
2025-05-18 21:09:30 +0530

    updated changes

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -102,7 +102,6 @@ export default function Navbar() {
                         <input type="email" placeholder='Enter your Email' />
                         <button className="sent">Sent Mail</button>
                         <input type="file" name="file" id="file" className='file'/>
-                        <input type="text"  />
                     </form>
                 </div>
             )}
